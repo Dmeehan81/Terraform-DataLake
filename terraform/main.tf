f9934cbd2e530dbd5385267cb0b62cf812d9de6a
Darren Meehan <DarrenMeehan@gmail.com>
2021-05-14 15:37:00 +0100

    dl filesystem instead of containers

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,19 +5,13 @@ data azurerm_resource_group "rg" {
 
 data azurerm_storage_account "DL" {
 
-    name                    = var.storage-account-name
-    resource_group_name     = data.azurerm_resource_group.rg.name
+    name                 = var.storage-account-name
+    resource_group_name  = data.azurerm_resource_group.rg.name
 }
 
-
-resource "azurerm_storage_container" "container" {
-  count                 = length(var.container-name)   
-  name                  = var.container-name[count.index]
-  storage_account_name  = data.azurerm_storage_account.DL.name
-  container_access_type = "private"
-}
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "lake" {
-  name               = "wbl-analytics"
+  count              = length(var.container-name)  
+  name               = var.container-name[count.index]
   storage_account_id = data.azurerm_storage_account.DL.id
 }
